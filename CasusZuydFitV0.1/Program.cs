ca77b5e678fa4126059be5960b79cfeb5abc6d87
LGZUYD <147499462+LGZUYD@users.noreply.github.com>
2024-04-04 10:08:42 +0200

    V.01

---
+++ CasusZuydFitV0.1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CasusZuydFitV0._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
