dd29d97aef4f8de916ba2264db679958c12b0bab
gabriel-lunesu <g.lunesu@outlook.com>
2024-04-04 12:57:06 +0200

    made a DAL

--- CasusZuydFitV0.1/obj/Debug/net8.0/CasusZuydFitV0.1.AssemblyInfo.cs
+++ CasusZuydFitV0.1/obj/Debug/net8.0/CasusZuydFitV0.1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CasusZuydFitV0.1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06c7a1ce995e02051b6c8a3b6a9771ee04411b9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0604fccd907011beeb35d790b9b4ddd636267b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CasusZuydFitV0.1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CasusZuydFitV0.1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
