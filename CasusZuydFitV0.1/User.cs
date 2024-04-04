0e25c74780f6906c7ff8e53570a6cea73225a548
LGZUYD <2305329goossens@zuyd.nl>
2024-04-04 10:44:39 +0200

    User class

---
+++ CasusZuydFitV0.1/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasusZuydFitV0._1
+{
+    class User
+    {
+        public string UserName { get; set; }
+        public User(string userName)
+        {
+            UserName = userName;
+        }
+    }
+}
