875f2e30b0843d45fcf9e6e66eff04586bd0e6e8
gabriel-lunesu <g.lunesu@outlook.com>
2024-04-05 11:40:40 +0200

    dependencies

--- CasusZuydFitV0.1/obj/Debug/net8.0/CasusZuydFitV0.1.AssemblyInfo.cs
+++ CasusZuydFitV0.1/obj/Debug/net8.0/CasusZuydFitV0.1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CasusZuydFitV0.1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cc5b89dc1ae030896e9e5ae3338335f00112887")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d5096282996664ae02cebc168b10a662bb8991d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CasusZuydFitV0.1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CasusZuydFitV0.1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
