3d91fda6cb15b221c61553a3f64939f8334fa091
LGZUYD <147499462+LGZUYD@users.noreply.github.com>
2024-04-05 10:25:17 +0200

    Create DAL.cs
    Fixed DAL directory

---
+++ CasusZuydFitV0.1/DAL.cs
@@ -0,0 +1,41 @@
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class DAL
+    {
+        private readonly string dbConString = "Server=tcp:gabriellunesu.database.windows.net,1433;Initial Catalog=ZuydFit;Persist Security Info=False;User ID=gabriellunesu;Password=3KmaCBt5nU4qZ4s%xG5@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+        public class UserDAL
+        {
+        }
+
+        public class ActivityDAL
+        {
+        }
+
+        public class AthleteDAL
+        {
+        }
+
+        public class EquipmentDAL
+        {
+        }
+
+        public class EventDAL
+        {
+        }
+
+        public class FeedbackDAL
+        {
+        }
+
+        public class TrainerDAL
+        {
+        }
+
+        public class EventorganisorDAL
+        {
+        }
+    }
+}
